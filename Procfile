e77acd3dc1a1c6da676e6a6fc0f9065f6d3b6663
ironsoul <timka2609@gmail.com>
2019-01-29 19:49:40 +0600

    scaled

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: python server.py
+worker: python main.py
--- server.py
+++
@@ -1,5 +0,0 @@
-from os import environ
-from flask import Flask
-
-app = Flask(__name__)
-app.run(host= '0.0.0.0', port=environ.get('PORT'))
